9c9bace06dec563fb167aaf59a5ceb4d5b2ba426
ehab fayed <efayed@damlag.com>
2026-03-04 13:23:04 +0200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_03_03_042049) do
+ActiveRecord::Schema[8.0].define(version: 2026_03_03_042713) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -93,6 +93,21 @@ ActiveRecord::Schema[8.0].define(version: 2026_03_03_042049) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["parentable_type", "parentable_id"], name: "index_contents_on_parentable"
+  end
+
+  create_table "faqs", force: :cascade do |t|
+    t.string "question_ar"
+    t.string "question_en"
+    t.text "answer_ar"
+    t.text "answer_en"
+    t.string "parentable_type"
+    t.bigint "parentable_id"
+    t.boolean "is_deleted", default: false
+    t.boolean "is_published", default: false
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["parentable_type", "parentable_id"], name: "index_faqs_on_parentable"
   end
 
   create_table "users", force: :cascade do |t|
